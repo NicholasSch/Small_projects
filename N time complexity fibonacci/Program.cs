3d842f02999efb19fc70ffb3601f8880cba71b01
NicholasSchreen <114251335+NicholasSch@users.noreply.github.com>
2024-06-29 20:21:27 -0300

    N time complexity fibonacci

---
+++ N time complexity fibonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+public class fib
+{
+    public static int fibo (int n)
+    {
+        int num1 = 0;
+        int num2 = 1;
+        int aux;
+        for (int i = 2;i <= n;i++)
+        {
+            aux = num2;
+            num2 = num1 +num2;
+            num1 = aux;
+        }
+        return num2;
+    }
+    static void Main (string[] args)
+    {
+        Console.WriteLine(fibo (6));
+    }
+}
